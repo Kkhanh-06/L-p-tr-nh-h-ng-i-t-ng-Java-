64cae65816983de745193751b135a13450508f67
kkhanh <khanhbpn12.ptit@gmail.com>
2025-09-22 09:58:18 +0700

    add Order.java

--- BT giua mon/shop_management/Order.java
+++ BT giua mon/shop_management/Order.java
@@ -1,3 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Order {
-    
+    private Customer customer;
+    private List<Product> products;
+    private int orderID;
+    private static int orderCount = 0;
+
+    public Order(Customer customer) {
+        this.customer = customer;
+        this.products = new ArrayList<>();
+        orderCount += 1;
+        this.orderID = orderCount;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public double caculateProductPrice() {
+        double total = 0;
+        for (Product product: products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public static int getOrderCount() {
+        return orderCount;
+    }
 }
